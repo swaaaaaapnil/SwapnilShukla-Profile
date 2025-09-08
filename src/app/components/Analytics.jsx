4e9c8ddd5678963e83d2c61938283e2f92333a0e
Anonymous <swapnilshukla470@gmail.com>
2025-09-08 23:15:05 +0530

    Add GA Analytics component

---
+++ src/app/components/Analytics.jsx
@@ -0,0 +1,44 @@
+"use client";
+import Script from "next/script";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+export function gaEvent(action, params = {}) {
+  window.gtag?.("event", action, params);
+}
+
+export default function Analytics() {
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (!GA_ID) return;
+    const url = pathname + (searchParams.size ? `?${searchParams}` : "");
+    window.gtag?.("config", GA_ID, {
+      page_path: url,
+      anonymize_ip: true,
+    });
+  }, [GA_ID, pathname, searchParams]);
+
+  if (!GA_ID) return null;
+
+  return (
+    <>
+      <Script
+        id="ga-src"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          window.gtag = gtag;
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', { anonymize_ip: true, page_path: window.location.pathname });
+        `}
+      </Script>
+    </>
+  );
+}
